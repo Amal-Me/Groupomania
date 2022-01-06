6dc236a5807161e8ed1ac48d59bb68b467f16cec
Amal <83458917+Amal-Me@users.noreply.github.com>
2022-01-06 21:08:43 +0100

    ajout frontend

---
+++ frontend/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+//association a l'élément html root
+ReactDOM.render(  
+    <App />,  
+  document.getElementById('root')
+
+);
