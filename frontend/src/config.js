30eebac80799f8a08092b9465854b77aa9f16479
Amal Mehalaine <amal.mehalaine@outlook.fr>
2025-04-02 09:28:56 +0200

    fix: correct export for URL

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 // frontend/src/config.js
-const API_BASE_URL = "http://groupomania.local/api"; // ou localhost:3000 en local si besoin
+export const API_BASE_URL = "http://groupomania.local/api"; // ou localhost:3000 en local si besoin
 
 export default API_BASE_URL;
